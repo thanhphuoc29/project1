fcb59b29278774b38ac4a3a1dadd77b619d7410f
thanhphuoc29 <84950188+thanhphuoc29@users.noreply.github.com>
2022-11-08 13:43:03 +0700

    Add files via upload

---
+++ InsertData.sql
@@ -0,0 +1,111 @@
+﻿USE QuanLyBanHang
+GO
+
+--them du lieu
+INSERT INTO dbo.KhachHang
+VALUES
+('KH01',N'Nguyễn Minh Tiến',20,N'Hà Nội',N'Nam'),
+('KH02',N'Chu Thị Yến',20,N'Thanh Hóa',N'Nữ'),
+('KH03',N'Nguyễn Tiến Thành',20,N'Nghệ An',N'Nam'),
+('KH04',N'Nguyễn Thị Quỳnh',20,N'Thái Bình',N'Nữ'),
+('KH05',N'Phùng Quang Tiến',20,N'Quảng Ninh',N'Nam'),
+('KH06',N'Nguyễn Thu Hường',20,N'Quảng Trị',N'Nữ'),
+('KH07',N'Chu Văn Mạng',20,N'Nam Định',N'Nam'),
+('KH08',N'Phùng Thị Minh Thư',20,N'Hà Nội',N'Nữ'),
+('KH09',N'Phạm Tiến Thành',20,N'Hà Nội',N'Nam'),
+('KH10',N'Nguyễn Thị Trang',20,N'Hà Nội',N'Nữ')
+GO
+INSERT INTO dbo.KhachHangSdt
+VALUES
+('KH01','095123415'),
+('KH02','096554165'),
+('KH03','033453123'),
+('KH04','036512355'),
+('KH05','091321551'),
+('KH06','064545551'),
+('KH07','045115155'),
+('KH08','093151321'),
+('KH09','091351535'),
+('KH10','064513155')
+GO
+INSERT INTO dbo.NhanVien
+VALUES
+('NV001',N'Nguyễn Việt Hoa',25,N'Phùng khoang',N'Nữ','CV01'),
+('NV002',N'Nguyễn Quốc Hưng',28,N'Thanh Xuân',N'Nam','CV01'),
+('NV003',N'Dinh Quốc Trung',30,N'Thanh Xuân Trung',N'Nam','CV02'),
+('NV004',N'Đỗ Duy Hưng',25, N'Đống Đa',N'Nam','CV02'),
+('NV005',N'Đào Thị Hoa',25, N'Thanh Xuân',N'Nữ','CV01'),
+('NV006',N'Cao Xuan Trung', 29 ,N'Nam Từ Liêm', 'Nam','CV01'),
+('NV007',N'Nguyễn Mai Linh', 27 ,N'Phùng Khoang',N'Nữ','CV01'),
+('NV008',N'Nguyễn Văn Trung',26,N'Long Biên','Nam','CV02'),
+('NV009',N'Vũ Đức Thọ', 35 ,N'Xuân Trường', 'Nam','CV02'),
+('NV010',N'Nguyễn Bảo Ngọc', 35 , N'Hà Đông', N'Nữ','CV01')
+GO
+
+
+INSERT INTO dbo.NhaCC
+VALUES
+('CC001',N'Công Ty TNHH Hà Long',N'Thanh Xuân - Hà Đông'),
+('CC002',N'Công Ty TNHH Hoàng Minh',N'Dương Nội - Hà Đông'),
+('CC003',N'Công Ty Kinh Doanh Việt Dương',N'Hoàng Mai - Hà Nội'),
+('CC004',N'Công Ty SX Thiên Long',N'Phủ Lý - Hà Nam'),
+('CC005',N'Công Ty TNHH Long Thành',N'Hai Bà Trưng - Hà Nội'),
+('CC006',N'Công Ty TNHH Bắc Việt',N'Cầu Giấy - Hà Nội'),
+('CC007',N'Công Ty Thực Phẩm Hương Ly',N' Từ Liêm - Hà Nội'),
+('CC008',N'Công Ty Thực Phẩm CP',N'Hoài Đức - Hà Nội'),
+('CC009',N'Công Ty TNHH Việt Đức',N'Thanh Xuân- Hà Đông'),
+('CC010',N'Công Ty TNHH Quang Long',N'Kiến An - Hải Phòng')
+GO
+
+
+INSERT INTO dbo.ChiNhanh
+VALUES
+('CN001','CC001','Chi Nhánh số 1','Thanh Xuân - Hà Đông'),
+('CN002','CC001','Chi Nhánh số 2','Yên Xá - Hà Đông'),
+('CN003','CC003','Chi Nhánh số 3','Hoàng Mai - Hà Nội'),
+('CN004','CC002','Chi Nhánh số 4','Dương Nội - Hà Đông'),
+('CN005','CC005','Chi Nhánh số 5','Hai Bà Trưng - Hà Nội'),
+('CN006','CC008','Chi Nhánh số 6','Dương Nội - Hà Đông'),
+('CN007','CC010','Chi Nhánh số 7','Kiến An - Hải Phòng'),
+('CN008','CC009','Chi Nhánh số 8','Thanh Xuân - Hà Đông'),
+('CN009','CC006','Chi Nhánh số 9','Cầu Giấy - Hà Nội'),
+('CN010','CC004','Chi Nhánh số 10','Phủ Lý - Hà Nam')
+GO
+
+INSERT INTO dbo.SanPham
+(
+    id_sp,
+    ten,
+    hang,
+    gia,
+    loai,
+    id_hd
+)
+VALUES
+(   '',  -- id_sp - char(10)
+    N'', -- ten - nvarchar(50)
+    N'', -- hang - nvarchar(50)
+    0.0, -- gia - real
+    N'', -- loai - nvarchar(50)
+    ''   -- id_hd - char(10)
+    )
+INSERT INTO dbo.NhapHang
+(
+    id_nhap,
+    id_cn,
+	id_sp,
+	id_nv,
+    so_luong
+)
+VALUES
+('NH001','CN01','SP001','NV01','100'),
+('NH002','CN02','SP001','NV02','90'),
+('NH003','CN03','SP001','NV03','66'),
+('NH004','CN04','SP001','NV04','78'),
+('NH005','CN05','SP001','NV05','125'),
+('NH006','CN06','SP001','NV06','15'),
+('NH007','CN07','SP001','NV07','70'),
+('NH008','CN08','SP001','NV08','95'),
+('NH009','CN09','SP001','NV09','65'),
+('NH010','CN10','SP001','NV10','25')
+
---
+++ SQLQueryqlbh.sql
@@ -0,0 +1,101 @@
+﻿CREATE DATABASE QuanLyBanHang
+GO
+
+USE QuanLyBanHang
+GO
+
+CREATE TABLE KhachHang(
+	id_KH CHAR(10) PRIMARY KEY,
+	ten nvarchar(50),
+	tuoi INT,
+	dia_chi nvarchar(50),
+	gioi_tinh nvarchar(20)
+)
+GO
+CREATE TABLE KhachHangSdt(
+	id_KH CHAR(10) REFERENCES KhachHang(id_KH),
+	sdt CHAR(20) PRIMARY KEY
+)
+GO
+CREATE TABLE SanPham(
+	id_sp CHAR(10) PRIMARY KEY,
+	id_hd CHAR(10) REFERENCES dbo.HoaDon(id_hd),
+	ten NVARCHAR(50),
+	hang NVARCHAR(50),
+	gia REAL,
+	loai NVARCHAR(50)
+)
+GO
+CREATE TABLE HangTieuDung(
+	id_sp_Hangtd CHAR(10) REFERENCES dbo.SanPham(id_sp),
+	loai NVARCHAR(50),
+	hsd DATE
+)
+GO
+CREATE TABLE DoGiaDung(
+	id_sp_DoGd CHAR(10) REFERENCES dbo.SanPham(id_sp),
+	loai NVARCHAR(50)
+
+)
+CREATE TABLE Mua(
+	id_KH CHAR(10) REFERENCES dbo.KhachHang(id_KH),
+	id_sp CHAR(10) REFERENCES dbo.SanPham(id_sp),
+	so_luong INT
+)
+GO
+CREATE TABLE NhanVien(
+	id_nv CHAR(10) PRIMARY KEY,
+	ten NVARCHAR(50),
+	tuoi INT,
+	dia_chi NVARCHAR(50),
+	gioi_tinh NVARCHAR(10)
+)
+GO
+CREATE TABLE NhanViensdt(
+	id_nv CHAR(10) REFERENCES NhanVien(id_nv),
+	sdt CHAR(20) PRIMARY key
+)
+GO
+CREATE TABLE HoaDon(
+	id_hd CHAR(10) PRIMARY KEY,
+	ten_sp NVARCHAR(50),
+	id_nv CHAR(10) REFERENCES dbo.NhanVien(id_nv),
+	ngay DATE,
+	so_luong_sp INT
+	tong_tien REAL,
+)
+GO
+CREATE TABLE ChucVu(
+	id_cv CHAR(10) PRIMARY KEY,
+	id_nv CHAR(10) REFERENCES dbo.NhanVien(id_nv),
+	ten NVARCHAR(50)
+)
+-- chinh sua bang 
+ALTER TABLE dbo.ChucVu
+DROP FK__ChucVu__id_nv__46E78A0C
+
+ALTER TABLE dbo.NhanVien
+ADD id_cv CHAR(10) REFERENCES dbo.ChucVu(id_cv)
+-- xong
+GO 
+CREATE TABLE NhaCC(
+	id_nhacc CHAR(10) PRIMARY KEY,
+	ten NVARCHAR(50),
+	dia_chi NVARCHAR(50),
+)
+GO
+CREATE TABLE ChiNhanh(
+	id_cn CHAR(10) PRIMARY KEY,
+	id_nhacc CHAR(10) REFERENCES dbo.NhaCC(id_nhacc),
+	ten NVARCHAR(50),
+	dia_chi NVARCHAR(50)
+)
+GO
+CREATE TABLE NhapHang(
+	id_nhap CHAR(10) PRIMARY KEY,
+	id_sp CHAR(10) REFERENCES dbo.SanPham(id_sp),
+	id_nv CHAR(10) REFERENCES dbo.NhanVien(id_nv),
+	id_cn CHAR(10) REFERENCES dbo.ChiNhanh(id_cn),
+	so_luong int
+)
+GO 
